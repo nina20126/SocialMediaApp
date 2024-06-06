0fed7677bac40bbf607bb7c5567d4008969acc00
nina20126 <nina20126@student.hamk.fi>
2024-06-06 07:40:27 +0300

    Project start | Api calls with Get

---
+++ Persistence/Migrations/20240605180534_seed-data.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class seeddata : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Activities",
+                newName: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Activities",
+                newName: "Name");
+        }
+    }
+}
