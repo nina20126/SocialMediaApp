4675c4f086f5a88fdc067ac391aa53dea6632b50
nina20126 <nina20126@student.hamk.fi>
2024-06-12 16:24:03 +0300

    CRUD operations

---
+++ Application/Core/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿
+
+using AutoMapper;
+using Domain;
+
+namespace Application.Core
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles() 
+        {
+            CreateMap<Activity, Activity>();
+        }
+
+    }
+}
